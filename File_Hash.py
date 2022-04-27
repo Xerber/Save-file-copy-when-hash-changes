cbb1517d3d2e33a8c26cfb1d2ff65c1ecd6d6754
Xerber <cherobuk.91@gmail.com>
2022-04-27 21:27:29 +0300

    Update File_Hash.py
    Removed unnecessary print.
    Added creation of the starting version of the file.
    The date and time it was modified is added to the file name.
    Changed check time from 5 seconds to 1

--- File_Hash.py
+++ File_Hash.py
@@ -2,36 +2,32 @@ import hashlib
 import os
 from shutil import copy
 from time import sleep
+from datetime import datetime
+
 
 #def wich calc hash_sum
 def md5(fname):
-    hash_md5 = hashlib.md5()
-    with open(fname, "rb") as f:
-        for chunk in iter(lambda: f.read(4096), b""):
-            hash_md5.update(chunk)
-    return hash_md5.hexdigest()
+  hash_md5 = hashlib.md5()
+  with open(fname, "rb") as f:
+    for chunk in iter(lambda: f.read(4096), b""):
+      hash_md5.update(chunk)
+  return hash_md5.hexdigest()
 #---
 
 path = input("Enter the path to the file: ")
 end_path = input("Enter the path for backup directory: ")
 if not os.path.exists(end_path):
-    os.makedirs(end_path)
-    print("End_path was missing but I will create :)")
-    
-mind = md5(path)#saving start hash_sum
-i=0#variable to exclude spam
-while True:
-    if(md5(path)==mind):
-        sleep(5)
-        if i==0:
-            i+=1
-            print('Listening..')
-        else:
-            pass
-    else:
-        i=0
-        s = path.split('\\')
-        copy(path, end_path+'\\'+s[-1])
-        mind = md5(path)
-        print("reWrite")
+  os.makedirs(end_path)
+  print("End_path was missing but I will create :)")
 
+filename = path.split('\\')[-1]
+copy(path, f'{end_path}\startfile_{filename}')
+
+mind = md5(path)#saving start hash_sum
+while True:
+  date = datetime.now().strftime('%d%m.%H%M%S')
+  if(md5(path)==mind):
+      sleep(1)
+  else:
+    copy(path, f'{end_path}\{date}{filename}')
+    mind = md5(path)
